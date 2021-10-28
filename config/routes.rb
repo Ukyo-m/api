005ca4fad4109cda4af039c40666af302f9fa369
MUkyo <mycolor.palette.bigstory@live.jp>
2021-10-28 14:11:23 +0900

    delete scaffold user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
-    end
+        end
   end
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
